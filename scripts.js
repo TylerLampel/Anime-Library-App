e711cfccf11e1a8e79459428ba78a6b3d9be9696
Tyler Lampel <lampel13@gmail.com>
2022-06-12 11:43:56 -0400

    event listeners for opacity on cards, attempting css styling

--- scripts.js
+++ scripts.js
@@ -1,24 +1,30 @@
+
+// create anime card //
 function createAnimeCard(anime) {
-        const root = document.getElementById('root')
-        const container = document.createElement('div')
-        container.setAttribute('class', 'row')
-        const animeCard = document.createElement('img')
-        animeCard.src = anime.images.jpg.image_url
-        root.appendChild(container)
-        root.appendChild(animeCard)
-    }
+    const root = document.getElementById('root')
+    const container = document.createElement('div')
+    container.setAttribute('class', 'column')
+    const animeCard = document.createElement('img')
+    animeCard.src = anime.images.jpg.image_url
+    root.appendChild(container)
+    container.appendChild(animeCard)
+    animeCard.addEventListener("mouseenter", event => {
+        event.target.style = "opacity: 1"
+    })
+    animeCard.addEventListener("mouseleave", event => {
+        event.target.style = "opacity: 0.8"
+    })
+}
 
+function expandAnimeCard(anime) {
+    let expandedCard = document.getElementById("expandedImg");
+    // let imgText = document.getElementById("imgtext");
+    expandedCard.src= img.src;
+    // imgText.innerHTML = imgs.alt;
+    expandedCard.parent.Element.style.display = "block";
+}
 
-
-//line
 
 fetch("https://api.jikan.moe/v4/anime")
 .then(response => response.json())
-.then(animes => animes.data.forEach(anime => createAnimeCard(anime)))
-
-// let animeInfo;
-// fetch("https://api.jikan.moe/v4/anime")
-// .then(response => response.json())
-// .then(animes => animeInfo = animes)
-// .then( () => animeInfo)
-
+.then(animes => animes.data.forEach(anime => console.log(anime, createAnimeCard(anime))))
