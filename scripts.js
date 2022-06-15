69f491bc77e46ae2f9d79ff5dc62f7da89b4706c
Tyler Lampel <lampel13@gmail.com>
2022-06-14 21:12:34 -0400

    finished basic product. all functions work correctly

--- scripts.js
+++ scripts.js
@@ -3,18 +3,17 @@ fetch("https://api.jikan.moe/v4/anime")
 .then(animes => animes.data.forEach(anime => createAnimeCard(anime), console.log(animes)))
   
 const modal = document.querySelector('.modal');
-const previews = document.querySelectorAll('.column img');
-const largeAnimeCard = document.querySelector('.large-img');
-const animeTitlle = document.querySelector('.caption')
+const previews = document.querySelectorAll('.img-container img');
+const largeAnimeCard = document.querySelector('.full-img');
+const animeTitle = document.querySelector('.caption')
+const gallery = document.querySelector('.gallery')
 
 
 // create anime card 
 function createAnimeCard(anime) {
-// holds all elements
-    const root = document.getElementById('root')
 // holds all images
     const container = document.createElement('div')
-    container.setAttribute('class', 'column')
+    container.setAttribute('class', 'img-container')
 // images
     const animeCard = document.createElement('img');
     animeCard.src = anime.images.jpg.image_url;
@@ -22,7 +21,7 @@ function createAnimeCard(anime) {
 // make images original data the large image
     animeCard.setAttribute('data-original', `${anime.images.jpg.large_image_url}`);
 //add image container to root
-    root.appendChild(container);
+    gallery.appendChild(container);
 //add images to container
     container.appendChild(animeCard);
 //change opacity of picture when hovered
@@ -31,24 +30,27 @@ function createAnimeCard(anime) {
     });
     animeCard.addEventListener("mouseleave", event => {
         event.target.style = "opacity: 0.8"
+    });  
+    animeCard.addEventListener("click", () => {
+        modal.classList.add("open");
+        largeAnimeCard.classList.add("open");
+        const largeImageSrc = animeCard.getAttribute("data-original");
+        largeAnimeCard.src = largeImageSrc;
+        const altText = animeCard.alt;
+        animeTitle.textContent = altText
     });
-};
-
-
-previews.forEach((preview) => {
-    preview.addEventListener("click", () => {
-    modal.classList.add("open");
-    largeAnimeCard.classList.add("open");
-    const largeImageSrc = preview.getAttribute("data-original");
-    largeAnimeCard.src = largeImageSrc;
-    const altText= preview.alt;
-    caption.textContent = altText;
+    modal.addEventListener('click', (event) => {
+        if(event.target.classList.contains("modal")) {
+            modal.classList.remove("open")
+            largeAnimeCard.classList.remove("open")
+        }
     })
-})
+}
 
-modal.addEventListener('click', (event) => {
-    if(event.target.classList.contains("modal")) {
-        modal.classList.remove("open")
-        largeAnimeCard.classList.remove("open")
-    }
-})
+
+// modal.addEventListener('click', (event) => {
+//     if(event.target.classList.contains("modal")) {
+//         modal.classList.remove("open")
+//         largeAnimeCard.classList.remove("open")
+//     }
+// })
